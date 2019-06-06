e1eda56218dc005ca14d02d038e059a23bbd35a1
unknown <meFermin216>
2019-06-06 10:18:30 -0700

    ya qedoo semilisto

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('styles'));
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+app.listen(3000,()=>{
+    console.log('Servidor corriendo en el puerto tri tausen');
+});
+
+app.get('/tablero',(req,res)=>{
+    res.render('tableroPrincipal.html');
+});
